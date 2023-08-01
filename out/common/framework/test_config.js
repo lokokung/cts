93027e681203479f05ec8d4e4e5c3b078830bcf5
lokokung <lokokung@gmail.com>
2023-08-01 20:26:17 +0000

    Deploying to gh-pages from @ lokokung/cts@ba2ff14a3f6aacebe49dec30f04dfec8c6552aed 🚀

---
+++ out/common/framework/test_config.js
@@ -0,0 +1,33 @@
+/**
+* AUTO-GENERATED - DO NOT EDIT. Source: https://github.com/gpuweb/cts
+**/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export const globalTestConfig = {
+  maxSubcasesInFlight: 500,
+  testHeartbeatCallback: () => {},
+  noRaceWithRejectOnTimeout: false,
+  unrollConstEvalLoops: false,
+  compatibility: false
+};
+//# sourceMappingURL=test_config.js.map
---
+++ out/common/internal/version.js
@@ -0,0 +1,3 @@
+// AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
+
+export const version = 'ba2ff14a3f6aacebe49dec30f04dfec8c6552aed';
